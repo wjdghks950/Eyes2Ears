d81b020accfc924170521cea8d4d1543d12ceadc
Jeonghwan <jeonghwankim123@gmail.com>
2019-05-19 03:58:57 +0900

    Update login page

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -7,11 +8,18 @@ import 'auth.dart';
 
 class LoginPage extends StatefulWidget {
   @override
-  _LoginPageState createState() => _LoginPageState();
+  State<StatefulWidget> createState() {
+    return _LoginPageState();
+  }
 }
 
 class _LoginPageState extends State<LoginPage> {
-  bool _success = false;
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    super.initState();
+  }
+    bool _success = false;
   String _uid;
 
   Future<FirebaseUser> _handleGoogleSignIn() async{
@@ -61,89 +69,222 @@ class _LoginPageState extends State<LoginPage> {
     return user;
   }
 
+  Future<FirebaseUser> _handleAnonymous() async{
+    final FirebaseUser user = await AuthService.auth.signInAnonymously();
+    assert(user != null);
+    assert(user.isAnonymous);
+    assert(!user.isEmailVerified);
+    assert(await user.getIdToken() != null);
+    if(Platform.isIOS){
+      // Anonymous auth doesn't show up as a provider on iOS
+      assert(user.providerData.isEmpty);
+    }
+    else if(Platform.isAndroid){
+      assert(user.providerData.length == 1);
+      assert(user.providerData[0].providerId == 'firebase');
+      assert(user.providerData[0].uid != null);
+    }
+
+    final FirebaseUser currentUser = await AuthService.auth.currentUser();
+    assert(user.uid == currentUser.uid);
+    setState((){
+      if(user != null){
+        _success = true;
+        _uid = user.uid;
+        AuthService.user = user;
+        print('Successfully signed in as a guest [uid]: ' + _uid);
+        Navigator.pop(context);
+        Firestore.instance.collection('accounts').document('anonymous').setData({
+            'uid' : user.email,
+            'isAnonymous': user.isAnonymous,
+            'displayName' : user.displayName,
+        });
+      }
+      else{
+        _success = false;
+      }
+    });
+    return user;
+  }
+
   @override
   Widget build(BuildContext context) {
-    String _googleLogoUrl = 'https://firebasestorage.googleapis.com/v0/b/mallapp-project.appspot.com/o/google-logo.png?alt=media&token=07f4a442-e727-4168-909a-0c9dc4786ace';
-    String _anonymousImgUrl = 'https://firebasestorage.googleapis.com/v0/b/mallapp-project.appspot.com/o/question-mark.png?alt=media&token=66e9497b-d8cf-475a-9d7d-dc21c0a1c729';
     return Scaffold(
-      body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 24.0),
-          children: <Widget>[
-            SizedBox(height: 80.0),
-            Column(
-              children: <Widget>[
-                Image.asset(
-                  'assets/diamond.png',
-                  color: Colors.black),
-                SizedBox(height: 16.0),
-                Text('JMall'),
-              ],
-            ),
-            SizedBox(height: 120.0),
-            // [Name]
-            ButtonTheme(
-              minWidth: 50.0,
-              child: RaisedButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5.0),
+      body: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/startpage_people.jpg"), 
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                color: Colors.red[200],
-                child: Container(
-                  child: Row(
-                    children: [
-                      Container(
-                        child: SizedBox(
-                          width: 30.0,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children:[
-                              Image.network(_googleLogoUrl),
-                            ],
-                          ),
+              ),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height/2.5,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.purple,
+                            Colors.purpleAccent,
+                          ],
                         ),
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(90)
+                        )
                       ),
-                      SizedBox(
-                        width: 50.0,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Spacer(),
+                          Align(
+                            alignment: Alignment.center,
+                            child: Icon(Icons.person,
+                              size: 90,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Spacer(),
+                        ],
                       ),
-                      Row(
-                        children:[
-                          Text('Google',
-                            style: TextStyle(color: Colors.white),
+                    ),
+
+                    Container(
+                      height: MediaQuery.of(context).size.height/2,
+                      width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.only(top: 62),
+                      child: Column(
+                        children: <Widget>[
+                          Material(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(50)
+                            ),
+                            child: InkWell(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(50)
+                              ),
+                              onTap: (){
+                                print("Google sign-in!");
+                                _handleGoogleSignIn().then((FirebaseUser user) => print(user)).catchError((e) => print(e)); //Pop up google sign in
+                              },
+                              child: 
+                              Container(
+                                width: MediaQuery.of(context).size.width/1.2,
+                                height: 100,
+                                //margin: EdgeInsets.only(top: 32),
+                                padding: EdgeInsets.only(
+                                    top: 4,left: 16, right: 16, bottom: 4
+                                ),
+                                decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    colors: [
+                                      Colors.purple,
+                                      Colors.purpleAccent,
+                                    ],
+                                  ),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black,
+                                      blurRadius: 4.0,
+                                    ),
+                                  ],
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(50)
+                                  )
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children:[ 
+                                    Image.asset(
+                                      "assets/google-logo.png",
+                                      scale: 5.0,  
+                                    ),
+                                    SizedBox(width: 20.0),
+                                    Center(
+                                      child: Text('Google Sign-in',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 30.0,
+                                          fontFamily: "Calibre-Semibold",
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 30.0),
+                          Material(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(50)
+                            ),
+                            child: InkWell(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(50)
+                              ),                    
+                              onTap: (){
+                                print("Login!");
+                                _handleAnonymous().then((FirebaseUser user) => print(user)).catchError((e) => print(e));
+                              },
+                              child: Container(
+                                height: 100,
+                                width: MediaQuery.of(context).size.width/1.2,
+                                decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    colors: [
+                                      Colors.purple,
+                                      Colors.purpleAccent,
+                                    ],
+                                  ),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.black,
+                                      blurRadius: 4.0,
+                                    ),
+                                  ],
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(50)
+                                  ),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children:[ 
+                                    Icon(Icons.person,
+                                      size: 50,
+                                      color: Colors.white,
+                                    ),
+                                    SizedBox(width: 20.0),
+                                    Center(
+                                      child: Text('Login'.toUpperCase(),
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 30.0,
+                                          fontFamily: "Calibre-Semibold",
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
                           ),
                         ],
                       ),
-                    ],
-                  ),
+                    )
+                  ],
                 ),
-                onPressed: (){
-                  _handleGoogleSignIn().then((FirebaseUser user) => print(user)).catchError((e) => print(e)); //Pop up google sign in
-                },
               ),
-            ),
-            SizedBox(height: 12.0),
-            ],
-          ),
-        ),
-      );
-    }
-}
-
-class AccentColorOverride extends StatelessWidget{
-  const AccentColorOverride({Key key, this.color, this.child}) : super(key : key);
-
-  final Color color;
-  final Widget child;
-
-  @override
-  Widget build(BuildContext context){
-    return Theme(
-      child: child,
-      data: Theme.of(context).copyWith(
-        accentColor: color,
-        brightness: Brightness.dark,
-      )
+        ],
+      ),
     );
   }
 }
