df51474331afd144cdf6eef421772265bfa588f7
Jeonghwan <jeonghwankim123@gmail.com>
2019-05-19 21:23:34 +0900

    Update menu page using LayoutBuilder and PageView.buidler

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,118 +1,329 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'auth.dart';
+import 'customIcons.dart';
+import 'data.dart';
+import 'dart:math';
 
-class MenuPage extends StatefulWidget{
-  _MenuPageState createState() => _MenuPageState();
+class MenuPage extends StatefulWidget {
+  @override
+  _MenuPageState createState() => new _MenuPageState();
 }
 
+var cardAspectRatio = 12.0 / 16.0;
+var widgetAspectRatio = cardAspectRatio * 1.2;
+
 class _MenuPageState extends State<MenuPage> {
+  var currentPage = images.length - 1.0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.person, color: Colors.white),
-          onPressed: () {
-            Navigator.pushNamed(context, '/profile');
-          }
-        ),
-        brightness: Brightness.light,
-        title: Text('Main',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20.0,
+    PageController controller = PageController(initialPage: images.length - 1);
+    controller.addListener(() {
+      setState(() {
+        currentPage = controller.page;
+      });
+    });
+
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [
+                Color(0xFF1b1e44),
+                Color(0xFF2d3447),
+              ],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+              tileMode: TileMode.clamp)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 12.0, right: 12.0, top: 30.0, bottom: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(
+                        CustomIcons.menu,
+                        color: Colors.white,
+                        size: 30.0,
+                      ),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                        size: 30.0,
+                      ),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Menu",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 46.0,
+                          fontFamily: "Calibre-Semibold",
+                          letterSpacing: 1.0,
+                        )),
+                    IconButton(
+                      icon: Icon(
+                        CustomIcons.option,
+                        size: 12.0,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {
+                        //TODO: Ascending / Descending order of menu items
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFFff6e6e),
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Center(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 22.0, vertical: 6.0),
+                          child: Text("Favorite menu",
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Stack(
+                children: <Widget>[
+                  CardScrollWidget(currentPage),
+                  Positioned.fill(
+                    child: PageView.builder(
+                      itemCount: images.length,
+                      controller: controller,
+                      reverse: true,
+                      itemBuilder: (context, index) {
+                        return Container();
+                      },
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("My Favorites",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 46.0,
+                          fontFamily: "Calibre-Semibold",
+                          letterSpacing: 1.0,
+                        )),
+                    IconButton(
+                      icon: Icon(
+                        CustomIcons.option,
+                        size: 12.0,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Center(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 22.0, vertical: 6.0),
+                          child: Text("People",
+                              style: TextStyle(color: Colors.white)),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    Text("My List",
+                        style: TextStyle(color: Colors.blueAccent))
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(left: 18.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: Image.asset("assets/startpage_people.jpg",
+                          width: 296.0, height: 222.0),
+                    ),
+                  )
+                ],
+              )
+            ],
           ),
         ),
-        centerTitle: true,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              Navigator.pushNamed(context, '/item');
-            },
-          ),
-        ],
-      ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Expanded(
-            child: StreamBuilder<QuerySnapshot>(
-              stream: Firestore.instance.collection('menulist').snapshots(),
-              builder: (context, snapshot){
-                if(!snapshot.hasData){
-                  return LinearProgressIndicator();
-                }
-                List<Product> products = snapshot.data.documents.map((product) => Product.fromSnapshot(product)).toList();
-                return GridView.count(
-                  crossAxisCount: 1,
-                  shrinkWrap: true,
-                  padding: EdgeInsets.all(16.0),
-                  childAspectRatio: 8.0 / 9.0,
-                  children: _buildGridCards(context, products),
-                );
-              },
-            ),
-          ),
-        ],
       ),
     );
   }
+}
 
-  List<Card> _buildGridCards(BuildContext context, List<Product> products){
-    if (products == null || products.isEmpty){ // In case the list of products is empty
-      return const <Card>[];
-    }
-    final ThemeData theme = Theme.of(context);
+class CardScrollWidget extends StatelessWidget {
+  var currentPage;
+  var padding = 20.0;
+  var verticalInset = 20.0;
 
-    return products.map((product){
-      return Card(
-        clipBehavior: Clip.antiAlias, // You can make it into a radius
-        elevation: 3.0,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-/*             AspectRatio(
-              aspectRatio: 18 / 12,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8.0),
-                child: Image.network(
-                  product.imgurl,
-                  fit: BoxFit.fitWidth,
-                ),
-              ),
-            ), */
-            Padding(
-              padding: EdgeInsets.fromLTRB(30.0, 5.0, 16.0, 0.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(height:10.0),
-                  Text(
-                    product == null ? '' : product.name,
-                    style: theme.textTheme.button,
-                    softWrap: false,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines:1,
+  CardScrollWidget(this.currentPage);
+
+  @override
+  Widget build(BuildContext context) {
+    return new AspectRatio(
+      aspectRatio: widgetAspectRatio,
+      child: StreamBuilder<QuerySnapshot>(
+        stream: Firestore.instance.collection('menulist').snapshots(),
+        builder: (context, snapshot){
+          if(!snapshot.hasData){
+            return LinearProgressIndicator();
+          }
+          List<Product> products = snapshot.data.documents.map((product) => Product.fromSnapshot(product)).toList();
+          return LayoutBuilder(
+            builder: (context, constraints) {
+              var width = constraints.maxWidth;
+              var height = constraints.maxHeight;
+
+              var safeWidth = width - 2 * padding;
+              var safeHeight = height - 2 * padding;
+
+              var heightOfPrimaryCard = safeHeight;
+              var widthOfPrimaryCard = heightOfPrimaryCard * cardAspectRatio;
+
+              var primaryCardLeft = safeWidth - widthOfPrimaryCard;
+              var horizontalInset = primaryCardLeft / 2;
+
+              List<Widget> cardList = new List();
+
+              for (var i = 0; i < products.length; i++) {
+                var delta = i - currentPage;
+                bool isOnRight = delta > 0;
+
+                var start = padding +
+                    max(
+                        primaryCardLeft -
+                            horizontalInset * -delta * (isOnRight ? 15 : 1),
+                        0.0);
+
+                var cardItem = Positioned.directional(
+                  top: padding + verticalInset * max(-delta, 0.0),
+                  bottom: padding + verticalInset * max(-delta, 0.0),
+                  start: start,
+                  textDirection: TextDirection.rtl,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(16.0),
+                    child: Container(
+                      decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                        BoxShadow(
+                            color: Colors.black12,
+                            offset: Offset(3.0, 6.0),
+                            blurRadius: 10.0)
+                      ]),
+                      child: AspectRatio(
+                        aspectRatio: cardAspectRatio,
+                        child: Stack(
+                          fit: StackFit.expand,
+                          children: <Widget>[
+                            Image.network(products[i].imgurl, fit: BoxFit.cover),
+                            Align(
+                              alignment: Alignment.bottomLeft,
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 16.0, vertical: 8.0),
+                                    child: Text(products[i].name,
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 50.0,
+                                            fontFamily: "SF-Pro-Text-Regular")),
+                                  ),
+                                  SizedBox(
+                                    height: 10.0,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 12.0, bottom: 12.0),
+                                    child: GestureDetector(
+                                      onTap: (){
+                                        // TODO: Navigate to corresponding page
+                                      },
+                                      child: Container(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 22.0, vertical: 6.0),
+                                      decoration: BoxDecoration(
+                                          color: Colors.blueAccent,
+                                          borderRadius: BorderRadius.circular(20.0)),
+                                      child: Text("Go to" + products[i].name,
+                                          style: TextStyle(color: Colors.white)),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      );
-    }).toList();
+                );
+                cardList.add(cardItem);
+              }
+              return Stack(
+                children: cardList,
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
 
 class Product{
   final String name;
+  final String imgurl;
 /*   final String category;
   final String description;
-  final String imgurl;
   final int likes;
   final double price;
   final String uid;
@@ -123,18 +334,18 @@ class Product{
 
   Product.fromMap(Map<String, dynamic> map, String documentID, {this.reference})
     : assert(map['name'] != null),
+      assert(map['imgurl'] != null),
 /*       assert(map['category'] != null),
       assert(map['description'] != null),
-      assert(map['imgurl'] != null),
       assert(map['likes'] != null),
       assert(map['price'] != null),
       assert(map['uid'] != null),
       assert(map['created'] != null),
       assert(map['modified'] != null), */
       name = map['name'],
+      imgurl = map['imgurl'],
 /*       category = map['category'],
       description = map['description'],
-      imgurl = map['imgurl'],
       likes = map['likes'],
       price = map['price'].toDouble(),
       uid = map['uid'],
@@ -148,19 +359,4 @@ class Product{
   @override
   String toString() => "<Product name: $name>";
 
-}
-
-Widget buildStar(BuildContext context, int numstars, bool detail){ // Generate random number of stars (from 1 to 5)
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.start,
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: List.generate(
-      numstars, 
-      (index) => Icon(
-        Icons.star, 
-        color: Colors.yellow, 
-        size: detail ? 25.0 : 10.0,
-      ),
-    ),
-  );
 }
