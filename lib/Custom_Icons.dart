540c7358ee67449e617a3b8fa266d55e4cfc01db
Jeonghwan <jeonghwankim123@gmail.com>
2019-05-24 01:52:01 +0900

    Add custom icons

---
+++ lib/Custom_Icons.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CustomIcons {
+  static const IconData search = IconData(0xe900, fontFamily: "CustomICons");
+  static const IconData menu = IconData(0xe901, fontFamily: "CustomICons");
+  static const IconData back_icon = const IconData(0xe900, fontFamily: "back_icon");
+}
