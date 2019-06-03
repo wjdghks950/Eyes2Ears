44a523139559098bad3458181eec8c591bc152a9
Jeonghwan <jeonghwankim123@gmail.com>
2019-06-03 09:52:48 +0900

    Update start page UI - New Icons and Fonts

--- lib/start.dart
+++ lib/start.dart
@@ -18,59 +18,28 @@ class StartPage extends StatelessWidget{
           Navigator.pushReplacementNamed(context, '/login');
         },
         child: Scaffold(
+          backgroundColor: Color(0xFF2d3447),
           body: Stack(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/startpage_people.jpg"), 
-                    fit: BoxFit.cover,
-                  ),
+            children: [
+              Positioned.fill(
+                top: MediaQuery.of(context).size.height / 3.0,
+                child: Column(
+                  children: [
+                    Icon(
+                      Icons.all_inclusive,
+                      color: Colors.white,
+                      size: MediaQuery.of(context).size.width / 10.0),
+                    //SizedBox(height: MediaQuery.of(context).size.height / 10.0),
+                    Text("Eyes2Ears",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'SF-Pro-Text-Bold',
+                        fontSize: MediaQuery.of(context).size.width / 10.0,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              SafeArea(
-                child: ListView(
-                  padding: EdgeInsets.symmetric(horizontal: 24.0),
-                  children: <Widget>[
-                    SizedBox(height: 100.0),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.all(32.0),
-                          child: Container(
-                            width: 120.0,
-                            height: 120.0,
-                            child: Stack(
-                              children: <Widget>[
-                                Container( 
-                                  decoration: BoxDecoration(
-                                    color: Colors.deepPurpleAccent.shade400,
-                                    shape: BoxShape.circle,
-                                  ),
-                                ),
-                                Container(
-                                  width: 100.0,
-                                  height: 100.0,
-                                  child: Text('E', 
-                                    style: TextStyle(
-                                      fontFamily: 'Kaushan Script' , //TODO: Need to import Kaushan Script font
-                                      color: Colors.white,
-                                      fontSize: 100.0,
-                                      fontStyle: FontStyle.italic,
-                                      ),
-                                    ),
-                                  alignment: Alignment(0.25, -0.3),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ], 
-                ),
-            ),
             ],
           ),
         ),
